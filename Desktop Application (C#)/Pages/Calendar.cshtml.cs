aac0ff518d0f4568002fdd29248d0de7e56a6aa1
matthewbothma <ST10041265@vcconnect.edu.za>
2025-09-22 13:08:46 +0200

    Recreated Git

---
+++ Desktop Application (C#)/Pages/Calendar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wilgproject.Pages
+{
+    public class CalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Any initialization or data fetching can be done here if needed
+        }
+    }
+}
